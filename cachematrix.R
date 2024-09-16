23f82d3ae7addf553cdc3477d0858cadeded01e4
LotusRcode <luxortus@gmail.com>
2024-09-16 17:42:47 -0400

    the final version of the ProgramAssigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##########################################################################
+## Functions for cashing the inverse of the matrix to save some CPU time.#
+##########################################################################
 
-## Write a short comment describing this function
+## 1. The first function from assignment that creates a cashed matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # assign inverse value
+  inv_x <- NULL
+  # set the original matrix and reset inverse
+  set <- function(y) {
+    x <<- y
+    inv_x <<- NULL
+  }
+  # get the original matrix
+  get <- function() x
+  # set inverse value
+  set_inv_x <- function(inv_func) inv_x <<- inv_func
+  # get inverse value
+  get_inv_x <- function() inv_x
+  
+  # Computed matrix:
+  
+  list(set = set, get = get,
+       set_inv_x = set_inv_x,
+       get_inv_x = get_inv_x)
 }
 
 
-## Write a short comment describing this function
+## 2. The second function from the assignment that computes the inverse of the object that returned by 
+## makeCacheMatrix. If the inverse matrix is cashed, then the function will used cashed object. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(computed_matrix, ...) {
+  inv_x <- computed_matrix$get_inv_x()
+  if(!is.null(inv_x)) {
+    message("cached data available!")
+    return(inv_x)
+  }
+  temp <- computed_matrix$get()
+  inv_x <- solve(temp, ...)
+  computed_matrix$set_inv(inv_x)
+  inv_x
 }
